8a0a68b4879fffb13a81ac7986a5fb8c4f6d1ac9
paulo.gomes@owl.ucc.edu <paulo.gomes@owl.ucc.edu>
2018-05-19 13:17:55 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,6 +3,5 @@ var playlist = new Object({artistName: 'songTitle'})
 
 function updatePlaylist(playlist, artistName, songTitle)
 {
-  var playlistUpdate = Object.assign({},playlist, {artistName: 'songTitle' })
-  return playlistUpdate
+  const 
 }
