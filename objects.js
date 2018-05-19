97466839c8ad1b02dbab53bb6e30c49b15666203
paulo.gomes@owl.ucc.edu <paulo.gomes@owl.ucc.edu>
2018-05-19 12:43:43 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,3 +1,7 @@
 var playlist = {
   TheScorpions: "Rock Me Like a Hurricane"
 }
+function updatePlaylist(playlist, artistName, songTitle)
+{
+  return Object.assign({}.playlist, artistName, songTitle)
+}
